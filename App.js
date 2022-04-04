a9171ea5b97d9ecc8733658e178482fa457e1771
Muhammad Daniyal Zafar <daniyalzafar836@gmail.com>
2022-04-05 04:08:15 +0500

    Forgot Password Screen Added

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StyleSheet } from "react-native";
 import EmailRecoveryScreen from "./app/screens/EmailRecoveryScreen";
+import ForgotPassword from "./app/screens/ForgotPassword";
 import LoginScreen from "./app/screens/LoginScreen";
 import ProfileScreen from "./app/screens/ProfileScreen";
 import ReferralCode from "./app/screens/ReferralCode";
@@ -14,7 +15,7 @@ export default function App() {
     //     <AppButton title="Test" />
     //   </View>
     // </Screen>
-    <ProfileScreen />
+    <ForgotPassword />
   );
 }
 
