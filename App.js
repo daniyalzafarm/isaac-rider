e70f526058f31b902f3042305a85154078cf1a62
Muhammad Daniyal Zafar <daniyalzafar836@gmail.com>
2022-04-05 01:23:39 +0500

    Login Screen Added

--- App.js
+++ App.js
@@ -3,15 +3,17 @@ import { StyleSheet, Text, View } from "react-native";
 import AppButton from "./app/components/AppButton";
 import AppText from "./app/components/AppText";
 import Screen from "./app/components/Screen";
+import LoginScreen from "./app/screens/LoginScreen";
 
 export default function App() {
   return (
-    <Screen style={styles.container}>
-      <View style={{ alignItems: "center" }}>
-        <AppText>Open up App.js to start working on your app!</AppText>
-        <AppButton title="Test" />
-      </View>
-    </Screen>
+    // <Screen style={styles.container}>
+    //   <View style={{ alignItems: "center" }}>
+    //     <AppText>Open up App.js to start working on your app!</AppText>
+    //     <AppButton title="Test" />
+    //   </View>
+    // </Screen>
+    <LoginScreen />
   );
 }
 
---
+++ app/screens/LoginScreen.js
@@ -0,0 +1,125 @@
+import React, { useState } from "react";
+import {
+  View,
+  StyleSheet,
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
+import AppButton from "../components/AppButton";
+import AppText from "../components/AppText";
+import Screen from "../components/Screen";
+import colors from "../config/colors";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+function LoginScreen(props) {
+  const [userId, setUserId] = useState();
+  const [password, setPassword] = useState();
+  const [isPasswordSecure, setIsPasswordSecure] = useState(true);
+  return (
+    <ScrollView>
+      <Screen>
+        <View style={styles.container}>
+          <View
+            style={{
+              alignItems: "center",
+              marginBottom: 100,
+            }}
+          >
+            <FontAwesome5
+              name="shopping-basket"
+              size={80}
+              color={colors.primary}
+            />
+            <AppText style={{ fontSize: 20 }}>RIDER</AppText>
+          </View>
+          <View style={styles.inputSection}>
+            <MaterialCommunityIcons
+              name="account"
+              size={24}
+              color={colors.primary}
+            />
+            <TextInput
+              style={styles.input}
+              type="text"
+              autoCapitalize="none"
+              autoCorrect={false}
+              placeholder="UserId"
+              value={userId}
+              onChangeText={(text) => setUserId(text)}
+            />
+          </View>
+
+          <View style={styles.inputSection}>
+            <MaterialCommunityIcons
+              name="lock-outline"
+              size={24}
+              color={colors.primary}
+            />
+            <TextInput
+              style={styles.input}
+              secureTextEntry={isPasswordSecure}
+              autoCapitalize="none"
+              autoCorrect={false}
+              placeholder="Password"
+              value={password}
+              onChangeText={(text) => setPassword(text)}
+            />
+            <TouchableOpacity
+              style={{ position: "absolute", right: 10, marginLeft: 10 }}
+              onPress={() => setIsPasswordSecure((value) => !value)}
+            >
+              <MaterialCommunityIcons
+                name={isPasswordSecure ? "eye-outline" : "eye-off-outline"}
+                size={24}
+                color={colors.secondary}
+              />
+            </TouchableOpacity>
+          </View>
+
+          <AppButton
+            title="Login"
+            style={{
+              marginVertical: 40,
+            }}
+            //   onPress={handleLogin}
+          />
+          <TouchableOpacity
+          //   onPress={() => {handleForgot}}
+          >
+            <AppText>Forgot Password?</AppText>
+          </TouchableOpacity>
+        </View>
+      </Screen>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: Dimensions.get("window").height,
+    backgroundColor: colors.white,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 10,
+  },
+  input: {
+    flex: 1,
+    paddingStart: 10,
+  },
+  inputSection: {
+    flexDirection: "row",
+
+    alignItems: "center",
+
+    borderBottomColor: colors.secondary,
+    borderBottomWidth: 0.5,
+
+    width: "70%",
+    paddingBottom: 5,
+    marginVertical: 10,
+  },
+});
+export default LoginScreen;
